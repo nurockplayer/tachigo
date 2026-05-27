2495bcb465d76459ad5d183a56c57ad73f9b6256
nu0915 <497f0013@gmail.com>
2026-05-27 20:32:30 +1000

    chore: normalize prototype extension asset names

--- design/prototypes/chrome-extension/src/assets/assets.js
+++ design/prototypes/chrome-extension/src/assets/assets.js
@@ -1,35 +1,35 @@
 export const ASSETS = {
-  opening01: "public/assets/first page/opening01.mp4",
-  opening02: "public/assets/first page/opening02.mp4",
-  logo: "public/assets/first page/logo.svg",
+  opening01: "public/assets/first-page/opening-01.mp4",
+  opening02: "public/assets/first-page/opening-02.mp4",
+  logo: "public/assets/first-page/logo.svg",
 
-  loginBackground: "public/assets/login/login_background.svg",
-  loginCharacter: "public/assets/login/login_character.svg",
-  loginSubtitle: "public/assets/login/login_subtitle.svg",
-  loginEmailInput: "public/assets/login/login_Input-email.svg",
-  loginPasswordInput: "public/assets/login/login_Input-password.svg",
-  loginButton: "public/assets/login/Login_Button.svg",
-  loginTwitchButton: "public/assets/login/Login_button-Twitch.svg",
-  loginOr: "public/assets/login/login_or.svg",
+  loginBackground: "public/assets/login/login-background.svg",
+  loginCharacter: "public/assets/login/login-character.svg",
+  loginSubtitle: "public/assets/login/login-subtitle.svg",
+  loginEmailInput: "public/assets/login/login-input-email.svg",
+  loginPasswordInput: "public/assets/login/login-input-password.svg",
+  loginButton: "public/assets/login/login-button.svg",
+  loginTwitchButton: "public/assets/login/login-button-twitch.svg",
+  loginOr: "public/assets/login/login-or.svg",
 
-  characterTitle: "public/assets/CharacterSelect/title.svg",
-  characterCrab: "public/assets/CharacterSelect/crab_character.mp4",
-  characterDiveButton: "public/assets/CharacterSelect/dive-in_button.svg",
-  characterLeftArrow: "public/assets/CharacterSelect/left_arrow.svg",
-  characterRightArrow: "public/assets/CharacterSelect/right_arrow.svg",
-  characterPageDots: "public/assets/CharacterSelect/page_icon.svg",
+  characterTitle: "public/assets/character-select/title.svg",
+  characterCrab: "public/assets/character-select/crab-character-loop.mp4",
+  characterDiveButton: "public/assets/character-select/dive-in-button.svg",
+  characterLeftArrow: "public/assets/character-select/left-arrow.svg",
+  characterRightArrow: "public/assets/character-select/right-arrow.svg",
+  characterPageDots: "public/assets/character-select/page-icon.svg",
 
-  crabSayHi: "public/assets/crab/say-hi/sayHi-loop.mp4",
+  crabSayHi: "public/assets/crab/say-hi/say-hi-loop.mp4",
   crabIdle: "public/assets/crab/idle/idle-loop.mp4",
   crabMining: "public/assets/crab/mining/mining-loop.mp4",
 
   hud: {
-    arrows: "public/assets/icon_button/arrows.svg",
-    cpc: "public/assets/icon_button/CPC_icon.svg",
-    level: "public/assets/icon_button/level_icon.svg",
-    miningSpeed: "public/assets/icon_button/mining_speed_icon.svg",
-    perClick: "public/assets/icon_button/pre_click_icon.svg",
-    settings: "public/assets/icon_button/setting_icon.svg"
+    arrows: "public/assets/icon-button/arrows.svg",
+    cpc: "public/assets/icon-button/cpc-icon.svg",
+    level: "public/assets/icon-button/level-icon.svg",
+    miningSpeed: "public/assets/icon-button/mining-speed-icon.svg",
+    perClick: "public/assets/icon-button/pre-click-icon.svg",
+    settings: "public/assets/icon-button/setting-icon.svg"
   }
 };
 
